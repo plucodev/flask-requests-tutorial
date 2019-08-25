74f796023968b0abab2d305d8b886b57bc81e5a3
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-08-25 22:49:24 +0000

    installed requests

---
+++ src/github.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+r = requests.get('https://api.github.com/users/plucodev')
+
+print(r.status_code)
+
+print(r.json)
+
+
+r = requests.get('hhttps://api.github.com/users/plucodev', stream=True)
+for line in r.iter_lines():
+    if line:
+        print(json.loads(line))
